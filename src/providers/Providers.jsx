1b2768632b2d170391ed6b85e7fde9d61032f3d5
Easin Arafat <arafatgw5@gmail.com>
2026-06-22 13:11:05 +0600

    Provider DOne

---
+++ src/providers/Providers.jsx
@@ -0,0 +1,25 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'sonner';
+import { useState } from 'react';
+
+export default function Providers({ children }) {
+    const [queryClient] = useState(() => new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 60 * 1000,
+                refetchOnWindowFocus: false,
+            },
+        },
+    }));
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+                <Toaster richColors position="top-center" />
+                {children}
+            </ThemeProvider>
+        </QueryClientProvider>
+    );
+}
